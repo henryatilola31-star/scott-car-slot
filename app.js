05c2b8fb77378cabe1b5d5495d08837dff367e89
henryatilola31-star <henryatilola31@gmail.com>
2026-09-14 13:40:48 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+//menu icon
+function openMen(){
+    document.getElementById('pass').style.display = 'none';
+    document.getElementById('puff').style.display = 'block';
+    document.getElementById('menulink').style.display = 'block';
+}
+function closemenu(){
+    document.getElementById('pass').style.display = 'block';
+    document.getElementById('puff').style.display = 'none';
+    document.getElementById('menulink').style.display = 'none';
+}
